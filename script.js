3def8e617f8f27062d77c1bcf4f30cbe64b9175f
Justin <JustinE@tlgcohort.com>
2024-07-08 11:48:48 -0500

    reverted back to exposed apiKey

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   let userInput = document.getElementById("userInput");
-  const api_key = secrets.GIPHY_API_KEY;
+  const api_key = "U57EjmM9fOth6BBbD3hrv8orByvwKG1T";
   const baseUrl = "https://api.giphy.com/v1/gifs/";
   let gifDiv = document.getElementById("gifList");
   let buttons = document.getElementsByTagName("button");
